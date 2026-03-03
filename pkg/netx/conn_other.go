0d602b97413e33b80d1e2e84f6f3732caa4faf83
Alex Geer <monoflash@gmail.com>
2026-03-03 17:46:42 +0300

    Edited based on remarks...

--- pkg/netx/conn_other.go
+++ pkg/netx/conn_other.go
@@ -5,24 +5,22 @@ package netx
 import "golang.org/x/sys/unix"
 
 // tuneSocket sets low-level socket options for performance.
-func tuneSocket(fd int) error {
+func tuneSocket(fd int) (err error) {
 	// SO_REUSEADDR and SO_REUSEPORT for fast restarts and load balancing
-	if err := unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_REUSEADDR, 1); err != nil {
-		return err
+	if err = unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_REUSEADDR, 1); err != nil {
+		return
 	}
-	if err := unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_REUSEPORT, 1); err != nil {
-		return err
+	if err = unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_REUSEPORT, 1); err != nil {
+		return
 	}
 
 	// Large socket buffers for high throughput
-	if err := unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_RCVBUF, TCPBufferSize); err != nil {
-		return err
+	if err = unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_RCVBUF, TCPBufferSize); err != nil {
+		return
 	}
-	if err := unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_SNDBUF, TCPBufferSize); err != nil {
-		return err
+	if err = unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_SNDBUF, TCPBufferSize); err != nil {
+		return
 	}
 
-	_ = unix.SetsockoptInt(fd, unix.IPPROTO_TCP, unix.TCP_NODELAY, 1)
-
-	return nil
+	return
 }
